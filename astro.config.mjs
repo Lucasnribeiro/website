0d71d86a6aa98a5b71131ba73d1c98364927d64b
Lucas <lucasmnribeiro@gmail.com>
2023-12-13 21:10:00 -0300

    features: adding taiwlind and base layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import sitemap from "@astrojs/sitemap";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), sitemap()]
+  integrations: [react(), markdoc(), sitemap(), tailwind()]
 });
