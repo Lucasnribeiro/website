3d0c0d7384a1a1e5630b32d7d6ba7b21104baeb8
Lucas <lucasmnribeiro@gmail.com>
2023-12-16 12:42:17 -0300

    feat: main layout

---
+++ src/components/home/index.ts
@@ -0,0 +1,8 @@
+export { default as Banner } from "./Banner.astro";
+export { default as GithubProjects } from "./GithubProjects.astro";
+export { default as LatestPost } from "./LatestPost.astro";
+export { default as Portfolio } from "./Curriculum.astro";
+export { default as Spotify } from "./Spotify.astro";
+export { default as Visitors } from "./Visitors.astro";
+export { default as Welcome } from "./Welcome.astro";
+export { default as WelcomeTrapeze } from "./WelcomeTrapeze.astro";
