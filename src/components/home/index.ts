e1bce5cf3ff7fc4a52301c35ca2b5069432894e0
Lucas <lucasmnribeiro@gmail.com>
2024-07-11 00:12:43 -0300

    adding gifs to the homepage and latestpost

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,7 +1,7 @@
 export { default as Banner } from "./Banner.astro";
 export { default as GithubProjects } from "./GithubProjects.astro";
 export { default as LatestPost } from "./LatestPost.astro";
-export { default as Portfolio } from "./Curriculum.astro";
+export { default as Portfolio } from "./Projects.astro";
 export { default as Spotify } from "./Spotify.astro";
 export { default as Visitors } from "./Visitors.astro";
 export { default as Welcome } from "./Welcome.astro";
