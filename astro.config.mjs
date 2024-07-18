4665db9193c56e9929ef5eb866c085a90c0c55ef
Lucas <lucasmnribeiro@gmail.com>
2024-07-18 11:53:45 -0300

    adding google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), sitemap(), tailwind(), mdx()],
+  integrations: [react(), markdoc(), sitemap(), tailwind(), mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   markdown: {
     extendDefaultPlugins: true,
     globalCustomProperties: {
